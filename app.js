ae3e37f4acba1eafdb7bab99192271e917b02fd1
Sergei Palmer <zigpalmer@gmail.com>
2021-12-22 20:06:17 +0000

    Starting set up of post route

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const port = process.env.PORT || 3001;
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/search', async (req, res) => {
+  const apiSearch = new Searcher,
+  const results = await apiSearch.search(req.body.query)
+  
+  res.send({results: results})
 })
 
 app.listen(port, () => {
